66ab88b1ae9c0ab084e32e16e1c269f03852737e
Oleg196-Ek <zhookov@mail.ru>
2024-11-25 19:57:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-team1_num = int(input())
-team2_num = int(input())
-print('В команде Мастера кода участников:%s!' % '5')
-print( "Итого сегодня в командах участников:%s и %s!" % (5,6))
-
-score_2 = int(input())
-team1_time = float(input())
-print('Команда Волшебники данных решила задач: {}!'.format(score_2))
-print( 'Волшебники данных решили задачи за {}c!'.format(team1_time))
-
-score_1 = int(input())
-print(f'Команды решили {score_1} и {score_2} задач')
-team2_time = float(input())
-task_total = score_1 + score_2
+team1_num = 'Мастера кода'
+team2_num = 'Волшебники данных'
+team1_count = 5
+team2_count = 6
+team1_time = 1552.512
+team2_time = 2153.31451
+score_1 = 40
+score_2 = 42
+tasks_total = score_1 + score_2
 time_total = team1_time + team2_time
-time_avg = time_total / task_total
-print(f'Сегодня было решено {task_total} задач, в среднем по {time_avg} секунды за задачу')
+time_avg = 45.2
+
 if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
-    print(f'Победа команды Мастера кода!')
+    result = 'Победа команды Мастера кода!'
 elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
-    print(f'Победа команды Волшебники Данных!')
+    result = 'Победа команды Волшебники Данных!'
 else:
-    print(f'Ничья!')
+    result = 'Ничья!'
+
+# %
+print('В команде %(name)s участников: %(count)x' % {'name': team1_num, 'count': team1_count})
+print('Итого сегодня в командах участников: %(first)x и %(second)x !' % {'first': team1_count,'second': team2_count})
+
+# format()
+print('Команда {} решила: {} задач'.format(team2_num, score_2))
+print('{} решили задачи за {} c !'.format(team2_num, team1_time))
+
+# f-строки
+print(f'Команды решили {score_1} и {score_2} задач.')
+print(f'Сегодня было решено {tasks_total} в среднем по {time_avg} секунды на задачу!')
+print(f'Результаты битвы: {result}')
+
 
